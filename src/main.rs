a6246e3ae5a5038a8904d0b38610c3490d4fd598
Jacob Walker <jacob.walker27+git@protonmail.com>
2025-04-30 23:54:56 +0100

    Clone quietly into dir based on repo URL
    Format is `~/src/<host>/<path>` so
    `git@github.com:rust-lang/rustlings.git` is cloned into
    `~/src/github.com/rust-lang/rustlings`

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-use std::env;
+use std::{env, fs};
 use std::io::{self, Write};
+use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    let clone_args = env::args().skip(1);
-    let path = env::current_dir().unwrap();
+    let arg_repo = env::args().nth(1).expect("expecting argument: repository");
+
+    let (host_part, path_part) = parse_repo_string(&arg_repo).expect("couldn't parse repository");
+
+    let home_dir = env::var("HOME").expect("$HOME isn't set");
+
+    let mut target_dir = PathBuf::from(home_dir);
+    target_dir.push("src");
+    target_dir.push(host_part);
+    target_dir.push(path_part);
+
+    fs::create_dir_all(&target_dir).expect("error creating clone directory");
+
+    println!("Cloning repository to {:?}...", &target_dir);
 
     let command_output = Command::new("git")
-        .current_dir(path)
         .arg("clone")
-        .args(clone_args)
+        .arg("--quiet")
+        .arg(arg_repo)
+        .arg(&target_dir)
         .output()
         .expect("error calling git");
 
@@ -19,4 +33,35 @@ fn main() {
     io::stderr()
         .write_all(&command_output.stderr)
         .expect("error writing to stderr");
+
+    if command_output.status.success() {
+        println!("🎉 Done!");
+    }
+}
+
+fn parse_repo_string(repo_str: &str) -> Result<(&str, &str), &str> {
+    let part_after_proto = match repo_str.split_once("://") {
+        None => repo_str,
+        Some((_, p)) => p
+    };
+
+    let (prefix, suffix) = match part_after_proto.split_once(':') {
+        Some(("", _)) | Some((_, "")) | None => return Err("unexpected format"),
+        Some((prefix, _)) if prefix.contains('/') => return Err("looks like local path"),
+        Some(parts) => parts,
+    };
+
+    let host_part = match prefix.split_once('@') {
+        None => prefix,
+        Some((_, host)) => host,
+    };
+
+    let path_part_trimmed = suffix.trim_start_matches('/');
+
+    let path_part = match path_part_trimmed.strip_suffix(".git") {
+        None => path_part_trimmed,
+        Some(p) => p,
+    };
+
+    Ok((host_part, path_part))
 }
