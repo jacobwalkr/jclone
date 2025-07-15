dd0590dca61225152a4e1013d04fed05aa6dce55
Jacob Walker <jacob.walker27+git@protonmail.com>
2025-07-15 22:52:18 +0100

    Move git call to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use std::{fs, path::PathBuf, process::Command};
+use std::io::{self, Write};
+
+pub fn git_clone(repo_str: &str, target_dir: &PathBuf) -> Result<(), String> {
+    fs::create_dir_all(&target_dir).expect("error creating clone directory");
+
+    let command_output = Command::new("git")
+        .arg("clone")
+        .arg("--quiet")
+        .arg(repo_str)
+        .arg(target_dir)
+        .output()
+        .expect("error calling git");
+
+    match io::stdout().write_all(&command_output.stdout) {
+        Ok(_) => (),
+        Err(_) => return Err(String::from("error writing to stdout"))
+    };
+
+    match io::stderr().write_all(&command_output.stderr) {
+        Ok(_) => (),
+        Err(_) => return Err(String::from("error writing to stderr"))
+    };
+
+    if command_output.status.success() {
+        Ok(())
+    }
+    else {
+        Err(String::from("git returned non-zero status code"))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 mod repository;
 
+use jclone::git_clone;
 use repository::Repository;
-use std::io::{self, Write};
 use std::path::PathBuf;
-use std::process::Command;
-use std::{env, fs};
+use std::{env};
 
 fn main() {
     let arg_repo = env::args().nth(1).expect("expecting argument: repository");
@@ -17,26 +16,10 @@ fn main() {
     target_dir.push(repository.host);
     target_dir.push(repository.path);
 
-    fs::create_dir_all(&target_dir).expect("error creating clone directory");
-
     println!("Cloning repository to {:?}...", &target_dir);
 
-    let command_output = Command::new("git")
-        .arg("clone")
-        .arg("--quiet")
-        .arg(arg_repo)
-        .arg(&target_dir)
-        .output()
-        .expect("error calling git");
-
-    io::stdout()
-        .write_all(&command_output.stdout)
-        .expect("error writing to stdout");
-    io::stderr()
-        .write_all(&command_output.stderr)
-        .expect("error writing to stderr");
-
-    if command_output.status.success() {
-        println!("🎉 Done!");
+    match git_clone(&arg_repo, &target_dir) {
+        Ok(_) => println!("🎉 Done!"),
+        Err(err) => println!("❌ Error: {}", err)
     }
 }
