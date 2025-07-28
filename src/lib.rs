40943f59330d581204a6abd235ad7ddce61f9ef2
Jacob Walker <jacob.walker27+git@protonmail.com>
2025-07-28 09:10:40 +0100

    Move all but the arg fetch from main to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
 use std::io::{self, Write};
 use std::{fs, path::PathBuf, process::Command};
 
+use crate::configuration::Configuration;
+use crate::repository::Repository;
+
+mod configuration;
+mod repository;
+
+pub fn jclone(repo_str: String) {
+    let config = Configuration::load();
+    let repository = Repository::try_from(&repo_str).expect("couldn't parse repository");
+    let target_dir = config.base_dir.join(repository.host).join(repository.path);
+
+    println!("Cloning repository to {:?}...", &target_dir);
+
+    match git_clone(&repo_str, &target_dir) {
+        Ok(_) => println!("🎉 Done!"),
+        Err(err) => println!("❌ Error: {}", err),
+    }
+}
+
 pub fn git_clone(repo_str: &str, target_dir: &PathBuf) -> Result<(), String> {
     fs::create_dir_all(target_dir).expect("error creating clone directory");
 
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-mod configuration;
-mod repository;
-
-use jclone::git_clone;
-use repository::Repository;
-use std::{env};
-
-use crate::configuration::Configuration;
+use jclone::jclone;
+use std::env;
 
 fn main() {
-    let config = Configuration::load();
-
     let arg_repo = env::args().nth(1).expect("expecting argument: repository");
-    let repository = Repository::try_from(&arg_repo).expect("couldn't parse repository");
-
-    let mut target_dir = config.base_dir.clone();
-    target_dir.push(repository.host);
-    target_dir.push(repository.path);
-
-    println!("Cloning repository to {:?}...", &target_dir);
-
-    match git_clone(&arg_repo, &target_dir) {
-        Ok(_) => println!("🎉 Done!"),
-        Err(err) => println!("❌ Error: {}", err),
-    }
+    jclone(arg_repo);
 }
